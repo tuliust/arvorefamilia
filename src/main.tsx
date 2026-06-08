82312c83475fc18769e5481698995d6ff03b5a1b
tuliust <tuliust@gmail.com>
2026-06-08 14:17:14 -0300

    mount mobile tree controls portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
+import { MobileTreeControlsPortal } from "./app/components/FamilyTree/MobileTreeControlsPortal.tsx";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <MobileTreeControlsPortal />
+  </>
+);
