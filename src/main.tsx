6da4c2b9c553b10f25b862072d6f6758707380de
tuliust <tuliust@gmail.com>
2026-06-08 14:18:47 -0300

    import mobile tree controls css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import { MobileTreeControlsPortal } from "./app/components/FamilyTree/MobileTreeControlsPortal.tsx";
 import "./styles/index.css";
+import "./styles/mobile-tree-controls.css";
 
 createRoot(document.getElementById("root")!).render(
   <>
