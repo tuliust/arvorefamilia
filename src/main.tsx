c5543c08c77fa8e231832858b6408f5b81183aed
tuliust <tuliust@gmail.com>
2026-06-08 14:21:44 -0300

    mount mobile user menu palette portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import { MobileTreeControlsPortal } from "./app/components/FamilyTree/MobileTreeControlsPortal.tsx";
+import { MobileUserMenuPalettePortal } from "./app/components/layout/MobileUserMenuPalettePortal.tsx";
 import "./styles/index.css";
 import "./styles/mobile-tree-controls.css";
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <MobileTreeControlsPortal />
+    <MobileUserMenuPalettePortal />
   </>
 );
