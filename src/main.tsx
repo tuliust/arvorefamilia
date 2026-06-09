1d84112a6052e860500e4d6ee5622944e05559b9
tuliust <tuliust@gmail.com>
2026-06-08 21:13:23 -0300

    chore: stop mounting mobile palette portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import { MobileTreeControlsPortal } from "./app/components/FamilyTree/MobileTreeControlsPortal.tsx";
-import { MobileUserMenuPalettePortal } from "./app/components/layout/MobileUserMenuPalettePortal.tsx";
 import "./styles/index.css";
 import "./styles/mobile-tree-controls.css";
 import "./styles/mobile-edit-profile.css";
@@ -10,6 +9,5 @@ createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <MobileTreeControlsPortal />
-    <MobileUserMenuPalettePortal />
   </>
 );
