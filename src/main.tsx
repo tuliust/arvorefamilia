7236d4e2130fd592c355808a5fdd7bdb3b2f73b2
tuliust <tuliust@gmail.com>
2026-06-08 21:30:19 -0300

    chore: load mobile tree line overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from "./app/App.tsx";
 import { MobileTreeControlsPortal } from "./app/components/FamilyTree/MobileTreeControlsPortal.tsx";
 import "./styles/index.css";
 import "./styles/mobile-tree-controls.css";
+import "./styles/mobile-tree-lines.css";
 import "./styles/mobile-edit-profile.css";
 
 createRoot(document.getElementById("root")!).render(
