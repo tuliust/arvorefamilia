a9dae6d417b3eca8d52e33503b85582ad2de9398
tuliust <tuliust@gmail.com>
2026-06-27 03:31:40 -0300

    feat: enable meus vinculos enhancements

--- src/app/pages/MeusVinculosMobileShortcutsPage.tsx
+++ src/app/pages/MeusVinculosMobileShortcutsPage.tsx
@@ -1,9 +1,11 @@
 import { MeusVinculos } from './MeusVinculos';
+import { MeusVinculosEnhancements } from './meus-vinculos/MeusVinculosEnhancements';
 import { MeusVinculosPetEditorPortal } from './meus-vinculos/MeusVinculosPetEditorPortal';
 
 export function MeusVinculosMobileShortcutsPage() {
   return (
     <div>
+      <MeusVinculosEnhancements />
       <MeusVinculosPetEditorPortal />
       <MeusVinculos />
     </div>
